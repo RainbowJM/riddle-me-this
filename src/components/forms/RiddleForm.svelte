8323bcf96d5c3a55c65267a59af88712d2d90bea
JendrickH <jendrickharts92@outlook.com>
2023-09-14 09:17:33 -0400

    container added

--- src/components/forms/RiddleForm.svelte
+++ src/components/forms/RiddleForm.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <form method="POST" action="?/guess">
-	<div class="w-full p-8 flex flex-col gap-8 md:max-w-lg">
+	<div class="w-full p-8 container flex flex-col container gap-8 md:max-w-lg">
 		<slot />
 	</div>
 </form>
