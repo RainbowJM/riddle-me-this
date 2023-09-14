918ce9074b37669386c0134c8508739fe5b6cd55
Guenton Brooks <Guenton.Brooks@ibis-management.com>
2023-09-14 08:51:31 -0400

    auto redirect if logged in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	import { auth } from "$lib/firebase/auth";
+	import { onAuthStateChanged } from "firebase/auth";
+
 	import LoginBackground from "../components/containers/LoginBackground.svelte";
 	import LoginForm from "../components/forms/LoginForm.svelte";
 
+	onMount(() => onAuthStateChanged(auth, (user) => user && goto('/home')));
 </script>
 
 <LoginBackground>
