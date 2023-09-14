1c47e9223589bfc7b31cdd6a8919d00143080b47
Clayton Olaria <clayton.olaria@gmail.com>
2023-09-14 08:57:11 -0400

    process changes

--- src/lib/firebase/process.js
+++ src/lib/firebase/process.js
@@ -22,10 +22,34 @@ function getRiddle() {
 }
 
 /**
+ * @param {string | number} id
+ */
+async function getWinPoints(id) {
+    let weekId = getWeekId()
+    const docRef = doc(db, 'Scores', weekId);
+    const docSnap = await getDoc(docRef);
+    let points
+    if (docSnap.exists()) {
+        let winnersToday = docSnap.data().Daily
+        points = 5 - winnersToday.length()
+        points = (points < 0) ? 0 : points
+
+
+    } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+        console.log("creating document");
+        let obj = new Map()
+        obj.set(id, 5)
+        await setDoc(docRef, { 'users': obj }, { merge: true })
+    }
+
+}
+/**
  * @param {string} id
  */
 async function getPoints(id) {
-    let weekId = getWeekid()
+    let weekId = getWeekId()
     const docRef = doc(db, 'Scores', weekId);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
@@ -41,52 +65,55 @@ async function getPoints(id) {
  * @param {number} points
  */
 async function setPoints(id, points) {
-    let weekId = getWeekid()
+    let weekId = getWeekId()
     const docRef = doc(db, 'Scores', weekId);
     await setDoc(docRef, { 'users': { id: points } }, { merge: true });
 }
 
 async function getTodayWinners() {
-    let weekId = getWeekid()
+    let weekId = getWeekId()
     const docRef = doc(db, 'Scores', weekId);
     const docSnap = await getDoc(docRef);
+    let winnersToday = null
     if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data().Daily);
+        winnersToday = docSnap.data().Daily
+        console.log("Document data:", winnersToday);
     } else {
         // docSnap.data() will be undefined in this case
         console.log("No such document!");
     }
-
+    return winnersToday
 }
 
 async function getWeekWinners() {
-    let weekId = getWeekid()
+    let weekId = getWeekId()
     const docRef = doc(db, 'Scores', weekId,);
-
+    let winnersWeek = null
     const docSnap = await getDoc(docRef);
+
     if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data().Weekly);
+        winnersWeek = docSnap.data().weekly
+        console.log("Document data:", winnersWeek);
     } else {
         // docSnap.data() will be undefined in this case
         console.log("No such document!");
     }
 }
 
-
-function getWeekid() {
+function getWeekId() {
     let date = new Date()
     let year = date.getFullYear()
     // Copy the date so we don't modify the original
     date = new Date(date);
 
     let startDate = new Date(date.getFullYear(), 0, 1);
-    var days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
+    var days = Math.floor((date.getTime() - startDate.getTime()) / (24 * 60 * 60 * 1000));
 
     var weekNumber = Math.ceil(days / 7);
 
     // Display the calculated result      
     console.log("Week number of " + date +
         " is :   " + weekNumber);
-    let weekId = year.toString() + weekNumber.toString()
+    let weekId = year.toString() + weekNumber.toString().padStart(2, '0')
     return weekId;
 }
