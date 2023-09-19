f4a3e174aa92c1d36ec394e8122f34caffbfdcd7
clayton <clayton.olaria@gmail.com>
2023-09-19 19:53:58 -0400

    disable form when all standings are set

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -24,7 +24,6 @@
 	onMount(() => (unWatchStandings = watchTodayStandings()));
 	onMount(() =>
 		todayWinnerListState.subscribe((list) => {
-			console.log(list);
 			state = list.length;
 		})
 	);
@@ -40,7 +39,7 @@
 <Card>
 	<RiddleForm>
 		<h1 class="text-2xl text-center">{data.question}</h1>
-		<TextInput placeholder="Write your answer" name="answer" />
+		<TextInput placeholder="Write your answer" name="answer" disabled={state > 4} />
 
 		<SubmitButton disabled={state > 4} />
 
@@ -67,8 +66,26 @@
 				<p class="text-lg text-center my-4">Don't Give up! Try Again!! 💪</p>
 			{/if}
 		</div>
+		{#if state > 4}
+			<div class="alert alert-info">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="stroke-current shrink-0 w-6 h-6"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span>All standings set for today, comeback tomorrow for a chance to earn more points.</span
+				>
+			</div>
+		{/if}
 	</RiddleForm>
-</Card>
+</Card>f
 
 <Card>
 	<StandingsTable />
